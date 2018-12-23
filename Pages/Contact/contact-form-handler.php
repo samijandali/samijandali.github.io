254cdac8a085e3b6b33d6663aaf6f81aabf0e03a
samijandali <samirifai18@gmail.com>
2018-12-22 20:48:45 -0500

    Update contact-form-handler.php

--- Pages/Contact/contact-form-handler.php
+++ Pages/Contact/contact-form-handler.php
@@ -1,16 +1,19 @@
 <?php
-	$result = "";
+
 	if (isset($_POST['submit]'])){
 
-		require('PHPMailer/PHPMailerAutoload.php');
+		require('phpmailer/PHPMailerAutoload.php');
 		$mail = new PHPMailer();
+
 		$mail->Port = 587;
 		$mail->SMTPAuth = true;
 		$mail->SMTPSecure = 'tls';
+		$mail->Host='smtp.gmail.com';
+
 		$mail->Username = 'photography.contactform.sami@gmail.com';
 		$mail->Password = 'S619v$L#xDPi';
 
-		$mail->setFrom($_POST['email'], $_POST['name']);
+		$mail->setFrom('$_POST['email'], $_POST['name']');
 		$mail->addAddress('photography@samijandali.com');
 		$mail->addReplyTo($_POST['email'],$_POST['name']);
 
@@ -20,7 +23,7 @@
 		if(!$mail->send()){
 			$result = "something went wrong";
 		}else{
-			$result = "something went right";
+			$result = 
 		}
 	}
 
